e56d56afd1f1a79cad4e08c0ba03650d2a01f473
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-25 14:20:27 -0500

    first commit

---
+++ src/components/Post.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./Post.css";
+import { Users } from '../Data';
+
+export default function Post({post, jsonusers}) {
+    return ( 
+        <div className='post'>
+            <div className='postWrapper'>
+                <div className='postTop'>
+                    <div className='postTopLeft'>
+                        {/* <img className='postProfilePic' src={Users.filter(u=>u.id === post.userId)[0].profilePicture} /> */}
+                        <img className='postProfilePic' src={Users.filter(u=>u.id === post.userId)[0].profilePicture} />
+                        {/* <span className='postProfileName'> {jsonusers.filter(u=>u.id === post.userId)[0].username} </span> */}
+                        <span className='postProfileName'> {Users.filter(u=>u.id === post.userId)[0].username} </span>
+                        <span className="postTime"> {post.date }</span>
+                    </div>
+                    <div className='postTopRight'>
+                        <button className='postEdit'> Edit</button>
+                    </div>
+                </div>
+                <div className='postMain'>
+                    <img className='postMainPic' src={post.photo}  />
+
+                </div>
+                <div className='postBot'>
+                    <div className='postBotLeft'>
+                        <span className='postMainCaptions'>{post?.desc} </span>
+                    </div>
+                    <div className='postBotRight'>
+                        <button className='postComment'> Comment</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
